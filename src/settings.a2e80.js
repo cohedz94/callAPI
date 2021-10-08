7264bb4111e44be041ce574922fdfb7313c42343
Trien Chieu <cohedz@gmail.com>
2021-10-08 16:36:54 +0700

    fix

---
+++ src/settings.a2e80.js
@@ -0,0 +1,24 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/home.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "a8581",
+        resources: "3eef7",
+        main: "adde5"
+    }
+};
